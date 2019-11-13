6827a5c11b6c18444a140a37fafd9636fcc67afc
KeijiroTajima <keijiro.tajima@epfl.ch>
2019-11-13 19:07:02 +0100

    modify server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=1y8kq5BgbhMQXi3RM3ckvdelPJpV2tbKv'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Bushling', 'Heavy', 'Pressed', 'Shredded', 'Turning']
 path = Path(__file__).parent
 
 app = Starlette()
